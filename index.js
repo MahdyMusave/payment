6e370d7aa724b354b3d46d1a7fdc6175803a1824
mahde13 <musavemahdy>
2024-05-11 04:46:54 -0800

    add url stripe and added more details

--- index.js
+++ index.js
@@ -23,13 +23,32 @@ app.post("/checkout", async (req, res) => {
         },
         quantity: 1,
       },
+      {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: "javaScript T-shirt",
+          },
+          unit_amount: 20 * 100,
+        },
+        quantity: 2,
+      },
     ],
     mode: "payment",
-    success_url: "http://localhost:3000/complete",
-    cancel_url: "http://localhost:3000/cancel",
+    success_url: `${process.env.BASE_URL}/complete`,
+    cancel_url: `${process.env.BASE_URL}/cancel`,
   });
 
-  console.log(session);
+  // console.log(session);
+  //axion
+  res.redirect(session.url);
+});
+app.get("/complete", (req, res) => {
+  res.send("Your payment was successfly");
+});
+app.get("/cancel", (req, res) => {
+  // res.send("payment cancel");
+  res.redirect("/");
 });
 
 app.listen(3000, () => console.log("Server statar on port 3000"));
